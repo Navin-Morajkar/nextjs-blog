0ee77b99e552bc88c057f17715fd47f39b1569bf
Navin Morajkar <navinsika@gmail.com>
2023-08-13 23:08:47 +0530

    hi update

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,7 @@ export default function Home( { allPostsData } ) {
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Navin. I'm a Front End Developer</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Hiiiii Afreeeeeeeeeeeen
         </p>
       </section>
 
